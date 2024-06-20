824e441f6af216bc19cb25946278049027caf2dc
IdinKhayami <idin.khanoom.khayami@gmail.com>
2024-06-21 00:10:24 +0200

    feat(components): added error component

--- src/components/error/Error.test.tsx
+++ src/components/error/Error.test.tsx
@@ -1,5 +1,41 @@
-describe('Component: loading component', () => {
-  it('should match the snapshot', () => {
-    throw new Error('Not implemented');
+import { render, screen } from '@testing-library/react';
+
+import { Error } from '@/components/error/Error';
+import { DEFAULT_ERROR_MESSAGE } from '@/utils/constants';
+
+describe('Component: error component', () => {
+  it('should render error by default', () => {
+    render(<Error dataTestId="testingError" />);
+
+    expect(screen.getByTestId('testingError')).toBeInTheDocument();
+    expect(screen.getByTestId('testingError')).toMatchSnapshot();
+  });
+
+  it('should not have error if hasError is false', () => {
+    render(<Error dataTestId="testingError" hasError={false} />);
+
+    expect(screen.queryByTestId('testingError')).toBeNull();
+  });
+
+  it('should be able to render default error message', () => {
+    render(<Error dataTestId="testingError" hasError={true} />);
+
+    expect(screen.getByTestId('testingError')).toHaveTextContent(
+      DEFAULT_ERROR_MESSAGE,
+    );
+  });
+
+  it('should be able to render specified error message', () => {
+    render(
+      <Error
+        dataTestId="testingError"
+        hasError={true}
+        errorMessage="Awesome Error"
+      />,
+    );
+
+    expect(screen.getByTestId('testingError')).toHaveTextContent(
+      'Awesome Error',
+    );
   });
 });
--- src/components/error/Error.tsx
+++ src/components/error/Error.tsx
@@ -1,3 +1,26 @@
-export const Error = () => {
-  return null;
+import clsx from 'clsx';
+
+import { ComponentBaseProps } from '@/types/global.types';
+import { DEFAULT_ERROR_MESSAGE } from '@/utils/constants';
+
+import styles from './error.module.scss';
+
+interface ErrorProps extends ComponentBaseProps {
+  hasError?: boolean;
+  errorMessage?: string;
+}
+
+export const Error = ({
+  dataTestId,
+  errorMessage = DEFAULT_ERROR_MESSAGE,
+  hasError = true,
+  className,
+}: ErrorProps) => {
+  if (!hasError) return null;
+
+  return (
+    <div data-testid={dataTestId} className={clsx([styles.error, className])}>
+      <p>{errorMessage}</p>
+    </div>
+  );
 };
