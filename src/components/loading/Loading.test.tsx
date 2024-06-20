cd756f9ef031fa3dad9e4dd6d7da5f87649675df
IdinKhayami <idin.khanoom.khayami@gmail.com>
2024-06-21 00:07:10 +0200

    feat(components): added loading component

--- src/components/loading/Loading.test.tsx
+++ src/components/loading/Loading.test.tsx
@@ -1,5 +1,23 @@
+import { render, screen } from '@testing-library/react';
+
+import { Loading } from '@/components/loading/Loading';
+
 describe('Component: loading component', () => {
-  it('should match the snapshot', () => {
-    throw new Error('Not implemented');
+  it('should match the snapshot with loading state', () => {
+    render(<Loading dataTestId="testingLoading" />);
+
+    const loading = screen.getByTestId('testingLoading');
+
+    expect(loading).toBeInTheDocument();
+    expect(loading).toMatchSnapshot();
+  });
+
+  it('should match the snapshot without loading state', () => {
+    render(<Loading dataTestId="testingLoading" isLoading={false} />);
+
+    const loading = screen.queryByTestId('testingLoading');
+
+    expect(loading).not.toBeInTheDocument();
+    expect(loading).toMatchSnapshot();
   });
 });
--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -1,3 +1,11 @@
-export const Loading = () => {
-  return null;
+import { ComponentBaseProps } from '@/types/global.types';
+
+interface LoadingProps extends ComponentBaseProps {
+  isLoading?: boolean;
+}
+
+export const Loading = ({ isLoading = true, dataTestId }: LoadingProps) => {
+  if (!isLoading) return null;
+
+  return <p data-testid={dataTestId}>Loading ...</p>;
 };
